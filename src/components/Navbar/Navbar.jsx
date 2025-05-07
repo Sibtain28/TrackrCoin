0c4eb770f4b7de63239f99778ebbf0a151c02bcb
Sibtain28 <you@example.comsibtainahmedqureshi28@gmail.com>
2025-05-07 13:07:51 +0530

    Coin Details Page Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import logo from '../../assets/logo.png' 
 import arrow from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -31,9 +32,11 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
+        <Link to={'/'}>
         <img src={logo} alt="" className='logo'/>
+        </Link>
         <ul>
-            <li>Home</li>
+            <Link to={'/'}><li>Home</li></Link>
             <li>Portfolio</li>
             <li>Converter</li>
             <li>News</li>
--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
+import { useParams } from 'react-router-dom' //coin id dhundne ke liye url se
+import { CoinContext } from '../../context/CoinContext';
 
 const Coin = () => {
-  return (
-    <div>
-        <h2>coin</h2>
-    </div>
-  )
-}
+
+  const {coinId} = useParams();
+  const [coinData,setCoinData]=useState()
+  const {currency} = useContext(CoinContext)
+ 
+  const fetchCoinData = async ()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-dFGmU3bMRwyGnUvyWRg2FMuq'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(res => res.json())
+      .then(res => setCoinData(res))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(()=>{
+    fetchCoinData()
+  },[currency])
+
+  if(coinData){
+    return (
+      <div className='coin'>
+          <div className="coin-name">
+            <img src={coinData.image.large} alt="" />
+            <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+          </div>
+      </div>
+    )
+  }
+  else{
+    return (
+      <div className='loader'>
+          <div className="spin"></div>
+      </div>
+    )
+  }
+  }
+  
 
 export default Coin
